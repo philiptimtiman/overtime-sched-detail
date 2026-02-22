ca84de07d953cbabeb6b7e2c0a3510461c4da69b
philiptimtiman <philiptimtiman@gmail.com>
2026-02-22 08:24:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://your-project.supabase.co";
-const SUPABASE_ANON_KEY = "your-anon-public-key";
+const SUPABASE_URL = "https://bisvmgjgyvludhqsvvcn.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpc3ZtZ2pneXZsdWRocXN2dmNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2OTI1ODMsImV4cCI6MjA4NzI2ODU4M30.eodsmChKwoEiPlNz9d2Ge3lAmO3BWfdKRWVRdgk9Xl8";
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let allEntries = [];
